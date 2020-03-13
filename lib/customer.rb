8e1613aad2313f8682b4c910691e70f88fac987d
christine <bumpouce@gmail.com>
2020-03-13 08:45:40 -0700

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Customer
+  attr_accessor :name, :age
+
+  @@all = []
   
+  def initialize (name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal (waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.customer == self}
+  end
+
+  def waiters 
+    list_of_waiters = []
+    meals.select { |meal| list_of_waiters << meal.waiter }
+    list_of_waiters
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,37 @@
 class Waiter
+    attr_accessor :name, :age
 
+    @@all = []
+    
+    def initialize (name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+    def new_meal (customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+    
+    def customer 
+        list_of_customers = []
+        meals.select { |meal| list_of_customers << meal.customer }
+        list_of_customers
+    end
+
+    def best_tipper
+        best_tipper = ""
+        best_tip = 0
+
+        meals.select {|meal| (best_tip = meal.tip; best_tipper = meal.customer) if meal.tip > best_tip }
+        best_tipper
+    end
 end
